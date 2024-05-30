83d36092aaece2df640b782cf4e6d688c01da640
0x4b404ec <78906604@qq.com>
2024-05-30 16:18:57 +0800

    0x4b404ec #

--- .github/workflows/upm_subtree_split.sh
+++ .github/workflows/upm_subtree_split.sh
@@ -1,14 +1,14 @@
-UPM_BRANCH_NAME="upm"
-SEMVER_REG_EXP="[0-9]+.[0-9]+.[0-9]+"
+# UPM_BRANCH_NAME="upm"
+# SEMVER_REG_EXP="[0-9]+.[0-9]+.[0-9]+"
 
-VERSION_JSON=$(cat ./Assets/Core/package.json | grep version)
-echo $VERSION_JSON
-VERSION=$(echo $VERSION_JSON | grep -Eo $SEMVER_REG_EXP)
-echo $VERSION
-echo "==============================================================="
-git gc
+# VERSION_JSON=$(cat ./Assets/Core/package.json | grep version)
+# echo $VERSION_JSON
+# VERSION=$(echo $VERSION_JSON | grep -Eo $SEMVER_REG_EXP)
+# echo $VERSION
+# echo "==============================================================="
+# git gc
 
-git fetch origin $UPM_BRANCH_NAME:$UPM_BRANCH_NAME
-git subtree split --prefix=Assets/Core --branch $UPM_BRANCH_NAME
-git tag $VERSION $UPM_BRANCH_NAME
-git push origin -f $UPM_BRANCH_NAME --tags
+# git fetch origin $UPM_BRANCH_NAME:$UPM_BRANCH_NAME
+# git subtree split --prefix=Assets/Core --branch $UPM_BRANCH_NAME
+# git tag $VERSION $UPM_BRANCH_NAME
+# git push origin -f $UPM_BRANCH_NAME --tags
